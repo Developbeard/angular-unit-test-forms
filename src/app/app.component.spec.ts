d58bacdaf2c7dd45188f2bced04e08e1731204f7
Developbeard <rodlbarrera@outlook.es>
2022-08-11 00:36:45 -0500

    change nameÂ

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,9 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-testing-component'`, () => {
+  it(`should have as title 'angular-unit-test-forms'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-testing-component');
+    expect(app.title).toEqual('angular-unit-test-forms');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Calculator } from './calculator';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-testing-component';
+  title = 'angular-unit-test-forms';
 
   ngOnInit(): void {
     const calculator = new Calculator();
